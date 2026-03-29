3b00656c6400a3efa3d97b56db1cf57b25a7b874
Dmitry <slackpadbull@gmail.com>
2026-03-29 10:42:09 +0500

    update card

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@ const cards = document.querySelectorAll('.card');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    console.log('clicked');
+
+    // активная кнопка
+    buttons.forEach(btn => {
+      btn.classList.remove('filters__btn--active');
+    });
+
+    button.classList.add('filters__btn--active');
+
+    const filter = button.dataset.filter;
+
+    // фильтрация карточек
+    cards.forEach(card => {
+
+      const category = card.dataset.category;
+
+      const isVisible = filter === 'all' || filter === category;
+
+      card.classList.toggle('hidden', !isVisible);
+
+    });
+
   });
 });
