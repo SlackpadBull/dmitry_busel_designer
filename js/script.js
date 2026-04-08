98502378ef5c515e7ed8dc9cc4d4a64766876097
Dmitry <slackpadbull@gmail.com>
2026-04-08 17:06:07 +0500

    open-card

--- js/script.js
+++ js/script.js
@@ -1,28 +1,56 @@
-const buttons = document.querySelectorAll('.filters__btn');
-const cards = document.querySelectorAll('.card');
+const buttons = document.querySelectorAll('.filters__btn')
+const cards = document.querySelectorAll('.card')
+const cardDetail = document.querySelector('.card-detail')
+const cardDetailBack = document.querySelector('.card-detail__back')
+const portfolioHeader = document.querySelector('.portfolio__header')
+const portfolioGrid = document.querySelector('.portfolio__grid')
 
+// Фильтрация карточек
 buttons.forEach(button => {
-  button.addEventListener('click', () => {
+    button.addEventListener('click', () => {
+        // активная кнопка
+        buttons.forEach(btn => {
+            btn.classList.remove('filters__btn--active')
+        })
 
-    // активная кнопка
-    buttons.forEach(btn => {
-      btn.classList.remove('filters__btn--active');
-    });
+        button.classList.add('filters__btn--active')
 
-    button.classList.add('filters__btn--active');
+        const filter = button.dataset.filter
 
-    const filter = button.dataset.filter;
+        // фильтрация карточек
+        cards.forEach(card => {
+            const category = card.dataset.category
 
-    // фильтрация карточек
-    cards.forEach(card => {
+            const isVisible = filter === 'all' || filter === category
 
-      const category = card.dataset.category;
+            card.classList.toggle('hidden', !isVisible)
+        })
+    })
+})
 
-      const isVisible = filter === 'all' || filter === category;
+// Открытие детального просмотра карточки
+cards.forEach(card => {
+    card.addEventListener('click', e => {
+        e.preventDefault()
 
-      card.classList.toggle('hidden', !isVisible);
+        const title = card.querySelector('.subtitle').textContent
+        const text = card.querySelector('.card__text').textContent
+        const img = card.querySelector('.card__img').src
 
-    });
+        document.querySelector('.card-detail__title').textContent = title
+        document.querySelector('.card-detail__text').textContent = text
+        document.querySelector('.card-detail__img').src = img
+        document.querySelector('.card-detail__img').alt = title
 
-  });
-});
+        cardDetail.classList.remove('hidden')
+        portfolioHeader.style.display = 'none'
+        portfolioGrid.style.display = 'none'
+    })
+})
+
+// Закрытие детального просмотра
+cardDetailBack.addEventListener('click', () => {
+    cardDetail.classList.add('hidden')
+    portfolioHeader.style.display = 'flex'
+    portfolioGrid.style.display = 'grid'
+})
